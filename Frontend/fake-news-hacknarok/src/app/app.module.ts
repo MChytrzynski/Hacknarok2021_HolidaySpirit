65bdfb4a04bbd2cfeebff1d75672b5f5a74084be
MChytrzynski <marcinchytrzynski@gmail.com>
2021-03-27 15:38:07 +0100

    Generate components

--- Frontend/fake-news-hacknarok/src/app/app.module.ts
+++ Frontend/fake-news-hacknarok/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
+import { NewsListComponent } from './news-list/news-list.component';
+import { NewsListItemComponent } from './news-list-item/news-list-item.component';
+import { NewsSettingsComponent } from './news-settings/news-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+    NewsListItemComponent,
+    NewsSettingsComponent
   ],
   imports: [
     BrowserModule,
