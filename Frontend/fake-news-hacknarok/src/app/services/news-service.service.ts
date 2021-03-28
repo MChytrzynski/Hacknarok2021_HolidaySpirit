5a7131aa9e47145779c11f3538c5e304d71024be
MChytrzynski <marcinchytrzynski@gmail.com>
2021-03-28 11:45:34 +0200

    Fixed displaying news

--- Frontend/fake-news-hacknarok/src/app/services/news-service.service.ts
+++ Frontend/fake-news-hacknarok/src/app/services/news-service.service.ts
@@ -8,16 +8,19 @@ export class NewsServiceService {
   constructor(public httpClient: HttpClient) {}
   public localNews: News[] = [];
   public tags:Tag[]=[];
+  exampleTags:Tag[]=[{id:0,tagname:'World'},{id:0,tagname:'Health'},{id:0,tagname:'Sport'},{id:0,tagname:'Politics'},{id:0,tagname:'Science'},{id:0,tagname:'Tech'},{id:0,tagname:'Religion'},{id:0,tagname:'Covid'},{id:0,tagname:'Finance'}];
   getNews(): Observable<News[]> {
     return this.httpClient.get<News[]>('http://localhost:8080/');
   }
   setTags(): void {
-    this.localNews.forEach(news=>{
-        news.tags.forEach(tag=>{
-            if(!this.tags.some(x=>x.tagname===tag.tagname)){
-                this.tags.push(tag);
-            }
+        this.localNews.forEach(x=>{
+            x.tags=[];
+            const tagNumber=Math.floor(Math.random() * 8); 
+            x.tags.push(this.exampleTags[tagNumber]);
+            const tagNumber2=Math.floor(Math.random() * 8); 
+            x.tags.push(this.exampleTags[tagNumber]);
         })
-    })
+        this.tags=this.exampleTags;
   }
 }
+
